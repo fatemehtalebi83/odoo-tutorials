0ae710cdcdba5a8b818a3975a7760c34ba26c081
F Talebi <f.talebi@dybug.ir>
2026-07-19 17:13:22 +0330

    [FIX]  A simple Card component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 import { Component, useState } from "@odoo/owl";
-import { Counter } from "./counter/counter"
-import { Card } from "./card/card"
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
