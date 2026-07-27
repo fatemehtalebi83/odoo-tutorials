368245bf1048221f803059bb4db7913615f36c9d
F Talebi <f.talebi@dybug.ir>
2026-07-27 14:00:06 +0330

    [IMP]  Persist the game state

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -1,11 +1,23 @@
 import { reactive } from "@odoo/owl";
 import { registry } from"@web/core/registry";
 import { ClickerModel } from "./clicker_model";
+import { browser } from "@web/core/browser/browser";
+
+const STORAGE_KEY = "awesome_clicker_state";
 
 export const clickerService ={
 	start(env){
-		const clicker = new ClickerModel();
+		const savedState = browser.localStorage.getItem(STORAGE_KEY);
 
+		let clicker = new ClickerModel();
+
+		if (savedState) {
+            const state = JSON.parse(savedState);
+            delete state.bus;
+            Object.assign(clicker, state);
+        }
+
+        clicker = reactive(clicker);
 
 		clicker.bus.addEventListener(
             "MILESTONE_1k",
@@ -24,6 +36,23 @@ export const clickerService ={
 			}
 		},
 			10000);
+
+		setInterval(() => {
+            browser.localStorage.setItem(
+                STORAGE_KEY,
+                JSON.stringify({
+                    clicks: clicker.clicks,
+                    level: clicker.level,
+                    clickBots: clicker.clickBots,
+                    bigBots: clicker.bigBots,
+                    power: clicker.power,
+                    pearTrees: clicker.pearTrees,
+                    cherryTrees: clicker.cherryTrees,
+                    pears: clicker.pears,
+                    cherries: clicker.cherries,
+                })
+            );
+		}, 10000);
 
 		setInterval(() => {
             clicker.pears += clicker.pearTrees;
