2c012e94cdb74eaa4aba5c8285a9a0af52da8227
F Talebi <f.talebi@dybug.ir>
2026-07-28 12:03:35 +0330

    [IMP]  Load some data

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,5 +1,6 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 export class GalleryController extends Component {
 	static template ="awesome_gallery.GalleryController";
@@ -7,4 +8,37 @@ export class GalleryController extends Component {
 	static components = {
 		Layout,
 	};
+
+	setup() {
+		this.orm = useService("orm");
+
+		this.state = useState({
+			records: [],
+		});
+
+		onWillStart(async () =>{
+			await this.loadImages(this.props.domain);
+		});
+
+		onWillUpdateProps(async (nextProps) => {
+			await this.loadImages(nextProps.domain);
+		});
+	}
+
+	async loadImages(domain) {
+		const { records } = await this.orm.webSearchRead(
+			this.props.resModel,
+			domain,
+			{
+				specification: {
+					[this.props.imageField]: {},
+				},
+				context: {
+					bin_size: true,
+				},
+			}
+		);
+
+		this.state.records = records;
+	}
 }
