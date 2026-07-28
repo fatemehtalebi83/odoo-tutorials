d6400d06896379a447623203eec41532b1b24de9
F Talebi <f.talebi@dybug.ir>
2026-07-28 16:23:26 +0330

    [IMP]  Display images

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,4 +1,5 @@
 import {Component} from "@odoo/owl";
+import { url } from "@web/core/utils/urls";
 
 export class GalleryRenderer extends Component {
 	static template = "awesome_gallery.GalleryRenderer";
@@ -7,4 +8,16 @@ export class GalleryRenderer extends Component {
 		model: Object,
 		imageField: String,
 	};
+
+	getImageUrl(record) {
+		if(!record[this.props.imageField]) {
+			return null;
+		}
+
+		return url("/web/image", {
+			model: this.props.model.resModel,
+			id: record.id,
+			field: this.props.imageField,
+		});
+	}
 }
