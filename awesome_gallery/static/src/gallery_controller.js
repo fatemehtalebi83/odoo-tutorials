ff366d2f7abc1080acfb08a91226d66cdb82e2fe
F Talebi <f.talebi@dybug.ir>
2026-07-28 10:24:19 +0330

    [IMP] Use the Layout component

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,5 +1,10 @@
 import { Component } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {
 	static template ="awesome_gallery.GalleryController";
+
+	static components = {
+		Layout,
+	};
 }
