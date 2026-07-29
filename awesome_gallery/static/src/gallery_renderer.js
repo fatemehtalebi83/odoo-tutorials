41338215c7af6e18228db40cd4af9a3cd405ef97
F Talebi <f.talebi@dybug.ir>
2026-07-29 09:30:25 +0330

    [IMP] Switch to form view on click

--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,5 +1,6 @@
 import {Component} from "@odoo/owl";
 import { url } from "@web/core/utils/urls";
+import { useService } from "@web/core/utils/hooks";
 
 export class GalleryRenderer extends Component {
 	static template = "awesome_gallery.GalleryRenderer";
@@ -7,7 +8,12 @@ export class GalleryRenderer extends Component {
 	static props ={
 		model: Object,
 		imageField: String,
+		switchView: Function,
 	};
+
+	setup() {
+		this.action = useService("action");
+	}
 
 	getImageUrl(record) {
 		if(!record[this.props.imageField]) {
@@ -18,6 +24,12 @@ export class GalleryRenderer extends Component {
 			model: this.props.model.resModel,
 			id: record.id,
 			field: this.props.imageField,
+		});
+	}
+
+	openRecord = (record) => {
+		this.action.switchView("form", {
+			resId: record.id,
 		});
 	}
 }
