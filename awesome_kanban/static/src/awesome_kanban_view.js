9a1cf1856f797c45017e5fc1ce42d55c0adb37f9
F Talebi <f.talebi@dybug.ir>
2026-08-09 10:09:06 +0330

    [IMP]  Create a new kanban view

--- awesome_kanban/static/src/awesome_kanban_view.js
+++ awesome_kanban/static/src/awesome_kanban_view.js
@@ -1,1 +1,10 @@
-// TODO: Define here your AwesomeKanban view
+import { registry } from "@web/core/registry";
+import { kanbanView } from "@web/views/kanban/kanban_view";
+import { AwesomeKanbanController } from "./kanban_controller";
+
+export const awesomeKanbanView = {
+    ...kanbanView,
+    Controller: AwesomeKanbanController,
+};
+
+registry.category("views").add("awesome_kanban", awesomeKanbanView);
