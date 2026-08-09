88a5ed75943ff47f451de32df4aff468694ffaf5
F Talebi <f.talebi@dybug.ir>
2026-08-09 12:41:27 +0330

    [IMP] Update the main kanban view

--- awesome_kanban/static/src/kanban_controller.js
+++ awesome_kanban/static/src/kanban_controller.js
@@ -1,6 +1,8 @@
 import { KanbanController } from "@web/views/kanban/kanban_controller";
 import { CustomerList } from "./customer_list";
 
+const isFromAwesomeKanban =
+	"isFromAwesomeKanban";
 
 export class AwesomeKanbanController extends KanbanController{
 	static template = "awesome_kanban.AwesomeKanbanController";
@@ -9,4 +11,27 @@ export class AwesomeKanbanController extends KanbanController{
 		...KanbanController.components,
 		CustomerList,
 	};
+
+	selectCustomer = (customer) => {
+        const partner_id = customer.id;
+        const partner_name = customer.name;
+
+        const customerFilters = this.env.searchModel.getSearchItems(
+            (searchItem) => searchItem.isFromAwesomeKanban
+        );
+
+        for (const customerFilter of customerFilters) {
+            if (customerFilter.isActive) {
+                this.env.searchModel.toggleSearchItem(customerFilter.id);
+            }
+        }
+
+        this.env.searchModel.createNewFilters([
+            {
+                description: partner_name,
+                domain: [["partner_id", "=", partner_id]],
+                [isFromAwesomeKanban]: true,
+            },
+        ]);
+    };
 }
