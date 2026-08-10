af65930f13701170592e62fef69049ec1fb059f6
F Talebi <f.talebi@dybug.ir>
2026-08-10 13:55:01 +0330

    [IMP]  Cache network calls, create a service

---
+++ awesome_dashboard/static/src/statistics_service.js
@@ -0,0 +1,20 @@
+import { registry } from "@web/core/registry";
+import { rpc } from "@web/core/network/rpc";
+import { memoize } from "@web/core/utils/functions";
+
+const statisticsService = {
+	start() {
+		const loadStatistics = memoize(
+			async() => {
+				console.log("RPC called!");
+				return await rpc("/awesome_dashboard/statistics");
+			}
+		);
+
+		return {
+			loadStatistics,
+		};
+	},
+};
+
+registry.category("services").add("awesome_dashboard.statistics", statisticsService);
